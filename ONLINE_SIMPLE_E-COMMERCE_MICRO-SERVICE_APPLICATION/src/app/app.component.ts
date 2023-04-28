6af10c0340021e8896fcfb8c954482d3d3e5550e
DINESH-SANKAR-SELVI <ddprofessional31@gmail.com>
2023-04-27 21:43:43 -0700

    setup Process

---
+++ ONLINE_SIMPLE_E-COMMERCE_MICRO-SERVICE_APPLICATION/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ONLINE_SIMPLE_E-COMMERCE_MICRO-SERVICE_APPLICATION';
+}
---
+++ ONLINE_SIMPLE_E-COMMERCE_MICRO-SERVICE_APPLICATION_BackEnd/src/main/java/com/MicroService/MainPackage/OnlineSimpleECommerceMicroServiceApplicationBackEndApplication.java
@@ -0,0 +1,17 @@
+package com.MicroService.MainPackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSimpleECommerceMicroServiceApplicationBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSimpleECommerceMicroServiceApplicationBackEndApplication.class, args);
+		
+		System.out.println("****************************************************************************************");
+		System.out.println("Boot Is Ready ...!");
+		System.out.println("****************************************************************************************");
+	}
+
+}
